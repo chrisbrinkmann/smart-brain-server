53b37520a2cd4ad67bbe5bdf197238aab6a39024
chrisbrinkmann <brinkmcd@gmail.com>
2019-08-29 02:55:54 -0400

    set connection string db

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const entry = require('./controllers/entry')
 const db = knex({
 	client: 'pg',
 	connection: {
-    host : process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   }
 });
